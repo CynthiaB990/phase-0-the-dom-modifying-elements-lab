ee28e96c4fe95412789b582b81f3a3e004f571c7
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-03-17 02:43:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const git = document.getElementById('main');
+git.remove('main');
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id",'victory')
+newHeader.innerHTML = "<h1>Cynthia is the champion<h1>"
